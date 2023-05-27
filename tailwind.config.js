6727d60674a974e0874008e590895d807b8dbb41
Aufarhan <farhanaufarr@gmail.com>
2023-05-27 23:04:47 +0700

    second commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}", "./node_modules/flowbite/**/*.js"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#0f4c82",
+        secondary: "#0f172a",
+        button: "#f59e0b",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [require("flowbite/plugin")],
+};
